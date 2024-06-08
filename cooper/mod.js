0aaf5a2e1ffaa0d25ed46202b1088639394e0c6e
Dave <dpaq_github@outrigr.com>
2024-06-08 16:03:00 -0400

    debugging

--- cooper/mod.js
+++ cooper/mod.js
@@ -34,6 +34,7 @@ const cooper = async (metaUrl) => {
   console.log(`\nHi from cooper`)
   // const callingLocation = dirname(fromFileUrl(metaUrl))
   const callingLocation = metaUrl
+  console.log(callingLocation)
 
   // get all files & necessary attributes
   console.log(`file.path's`)
